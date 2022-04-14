89dc9ca46e53f1432aa17ea6bdfcc26dc6b294dc
seungdang123 <seungdang123@gmail.com>
2022-04-15 07:43:11 +0900

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  outout: {
+  output: {
     filename: "bundle.js",
     path: path.resolve(__dirname + "/build"),
   },
