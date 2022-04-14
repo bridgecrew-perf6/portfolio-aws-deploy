3be7f5baa7d0acd8daf588722a27f9f6b94176ce
seungdang123 <seungdang123@gmail.com>
2022-04-15 08:34:54 +0900

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
-const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname + "/build"),
+    path: "./build",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|pages)/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
     ],
+    plugins: [new HtmlWebpackPlugin()],
   },
 };
