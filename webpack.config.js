ac86ecf6e212dd370dbe42373f1a7bd1138d0d3e
seungdang123 <seungdang123@gmail.com>
2022-04-15 08:44:48 +0900

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/test.js",
   output: {
-    path: __dirname + "/build",
     filename: "bundle.js",
+    path: path.resolve(__dirname + "/build"),
   },
+  mode: "none",
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html", // public/index.html 파일을 읽는다.
+      filename: "index.html", // output으로 출력할 파일은 index.html 이다.
+    }),
+  ],
 };
